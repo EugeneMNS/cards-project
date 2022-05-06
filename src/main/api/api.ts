683883884a056ced2d2f6848296079127d3b68df
EugeneMNS <evgen.str4@gmail.com>
2022-05-06 16:37:12 +0300

    First merge

--- src/main/api/api.ts
+++ src/main/api/api.ts
@@ -6,7 +6,8 @@ export type ResponseType = {
 }
 
 export const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    //baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
     withCredentials: true,
 })
 

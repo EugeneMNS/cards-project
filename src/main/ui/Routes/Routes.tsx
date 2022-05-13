19481f0444808558b8ff4cb0b4285b4ef24a695b
srgbar <srg.bar@yandex.ru>
2022-05-13 17:22:31 +0300

    fix: CreateNewPassword (token error)

--- src/main/ui/Routes/Routes.tsx
+++ src/main/ui/Routes/Routes.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {  Route, Routes } from "react-router-dom";
-import { Error404 } from "../Error404/Error404";
-import { Registration } from "../Registration/Registration";
-import { ProfilePage } from "../Profile/ProfilePage";
-import { ResetPassword } from "../ResetPassword/ResetPassword";
-import { CreateNewPassword } from "../CreateNewPassword/CreateNewPassword";
-import { SuperComponents } from "../SuperConponents/SuperComponents"
-import  Login  from "../Login/Login";
+import {Route, Routes} from "react-router-dom";
+import {Error404} from "../Error404/Error404";
+import {Registration} from "../Registration/Registration";
+import {ProfilePage} from "../Profile/ProfilePage";
+import {ResetPassword} from "../ResetPassword/ResetPassword";
+import {CreateNewPassword} from "../CreateNewPassword/CreateNewPassword";
+import {SuperComponents} from "../SuperConponents/SuperComponents"
+import Login from "../Login/Login";
 
 export enum Path {
     Login = "/login",
@@ -16,27 +16,24 @@ export enum Path {
     SuperComponents = "/superComponents",
     Error404 = "/404",
     FORGOT_PATH = '/forgot',
-   // Register = "/registration",
-
+    // Register = "/registration",
 }
+
 export const Register = "/registration"
 
 export const RoutesConst = () => {
     return (
         <div>
             <Routes>
-                <Route path="/" element={<ProfilePage/>} />
-                <Route path={Path.Login} element={<Login />} />
-                <Route path={Register} element={<Registration />} />
-                <Route path={Path.Profile} element={<ProfilePage />} />
-                <Route path={Path.ResetPassword} element={<ResetPassword />} />
-                <Route
-                    path={Path.CreateNewPassword}
-                    element={<CreateNewPassword />}
-                />
-                <Route path={Path.SuperComponents} element={<SuperComponents />} />
-                <Route path={Path.Error404} element={<Error404 />} />
-                <Route path="*" element={Path.Error404} />
+                <Route path="/" element={<ProfilePage/>}/>
+                <Route path={Path.Login} element={<Login/>}/>
+                <Route path={Register} element={<Registration/>}/>
+                <Route path={Path.Profile} element={<ProfilePage/>}/>
+                <Route path={Path.ResetPassword} element={<ResetPassword/>}/>
+                <Route path={Path.CreateNewPassword + '/:token'} element={<CreateNewPassword/>}/>
+                <Route path={Path.SuperComponents} element={<SuperComponents/>}/>
+                <Route path={Path.Error404} element={<Error404/>}/>
+                <Route path="*" element={Path.Error404}/>
             </Routes>
         </div>
     );
